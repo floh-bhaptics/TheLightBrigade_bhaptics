3af9a55559ea1ebe3db25b5fca916a49d3ecb997
floh-bhaptics <floh@fahrenbergers.de>
2023-02-17 11:31:16 +0100

    Version Bump

--- TheLightBrigade_bhaptics.cs
+++ TheLightBrigade_bhaptics.cs
@@ -10,7 +10,7 @@ using LB;
 using UnityEngine;
 using Unity.Mathematics;
 
-[assembly: MelonInfo(typeof(TheLightBrigade_bhaptics.TheLightBrigade_bhaptics), "TheLightBrigade_bhaptics", "1.0.5", "Florian Fahrenberger")]
+[assembly: MelonInfo(typeof(TheLightBrigade_bhaptics.TheLightBrigade_bhaptics), "TheLightBrigade_bhaptics", "1.0.6", "Florian Fahrenberger")]
 [assembly: MelonGame("Funktronic Labs", "The Light Brigade")]
 
 
